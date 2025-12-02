d5ca55d7809f62ea8aefc0abc0ee170f6fbdb049
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-02 16:37:54 -0500

    Fix issue with disabling notifications

--- app/Filament/Resources/Customers/Pages/CreateCustomer.php
+++ app/Filament/Resources/Customers/Pages/CreateCustomer.php
@@ -4,8 +4,14 @@ namespace App\Filament\Resources\Customers\Pages;
 
 use App\Filament\Resources\Customers\CustomerResource;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Notifications\Notification;
 
 class CreateCustomer extends CreateRecord
 {
     protected static string $resource = CustomerResource::class;
+
+    protected function getCreatedNotification(): ?Notification
+    {
+        return null;
+    }
 }
--- app/Filament/Resources/Customers/Pages/ListCustomers.php
+++ app/Filament/Resources/Customers/Pages/ListCustomers.php
@@ -13,8 +13,7 @@ class ListCustomers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make()
-              ->successNotification(null),
+            CreateAction::make(),
         ];
     }
 }
