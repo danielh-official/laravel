20c6050ef1a0b7c394aea8bd11b1a2f8b4da32e0
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-02 15:19:24 -0500

    Configure placeholder for TextEntry and TextColumn components

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Tables\Columns\TextColumn;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TextEntry::configureUsing(static function (TextEntry $field): void {
+            $field->placeholder('-');
+        });
+
+        TextColumn::configureUsing(static function (TextColumn $column): void {
+            $column->placeholder('-');
+        });
     }
 }
