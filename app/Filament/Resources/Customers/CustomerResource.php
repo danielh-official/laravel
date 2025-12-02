391e045234d31d79477a59b7dab777bc01e67115
Daniel Haven <49914607+danielh-official@users.noreply.github.com>
2025-12-02 16:45:47 -0500

    Switch to modal create to highlight that successNotification(null) doesn't work but successNotificationTitle(null) does

--- app/Filament/Resources/Customers/CustomerResource.php
+++ app/Filament/Resources/Customers/CustomerResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Resources\Customers;
 
-use App\Filament\Resources\Customers\Pages\CreateCustomer;
+// use App\Filament\Resources\Customers\Pages\CreateCustomer;
 use App\Filament\Resources\Customers\Pages\EditCustomer;
 use App\Filament\Resources\Customers\Pages\ListCustomers;
 use App\Filament\Resources\Customers\Schemas\CustomerForm;
@@ -41,7 +41,7 @@ class CustomerResource extends Resource
     {
         return [
             'index' => ListCustomers::route('/'),
-            'create' => CreateCustomer::route('/create'),
+            // 'create' => CreateCustomer::route('/create'),
             'edit' => EditCustomer::route('/{record}/edit'),
         ];
     }
--- app/Filament/Resources/Customers/Pages/ListCustomers.php
+++ app/Filament/Resources/Customers/Pages/ListCustomers.php
@@ -13,7 +13,9 @@ class ListCustomers extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            CreateAction::make(),
+            CreateAction::make()
+              ->successNotificationTitle(null)
+              // ->successNotification(null),
         ];
     }
 }
